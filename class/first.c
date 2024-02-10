8b5b85cc96da28c32bc26a5147a887d02ade90ea
fistonuzabumwana <youfistalline11@gmail.com>
2024-02-10 21:08:58 +0200

    C program class

---
+++ class/first.c
@@ -0,0 +1,12 @@
+//welcome to my 1st c program
+#include<stdio.h>
+void main ()
+{
+    //printf("Hello class!\n");
+    //printf("\tHello class!");
+    //getchar();
+    //printf("*\n*\t*\n*\t*\t*\n*\t*\t*\t*");
+    printf("*\t\t\t\t*\n*\t*\t\t*\t*\n*\t*\t*\t*\t*\n*\t*\t\t*\t*\n*\t\t\t\t*");
+    getchar();
+    return;
+}
---
+++ class/variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main()
+{
+    int a,b,c;
+
+    printf("enter a 2 numbers: \n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c=a+b;
+    printf("%d + %d = %d\n",a,b,c);
+    c=a-b;
+    printf("%d - %d = %d\n",a,b,c);
+    c=a*b;
+    printf("%d x %d = %d\n",a,b,c);
+    c=a/b;
+    printf("%d / %d = %d",a,b,c);
+
+}
