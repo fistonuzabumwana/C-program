0c6f3ac7271b8ffb2e4f501bb00d18ad9ce20118
fistonuzabumwana <youfistalline11@gmail.com>
2024-02-10 21:38:41 +0200

    C program no 1

--- class/first.c
+++ class/first.c
@@ -2,8 +2,8 @@
 #include<stdio.h>
 void main ()
 {
-    //printf("Hello class!\n");
-    //printf("\tHello class!");
+    printf("Hello class!\n");
+    printf("\tHello class!");
     //getchar();
     //printf("*\n*\t*\n*\t*\t*\n*\t*\t*\t*");
     printf("*\t\t\t\t*\n*\t*\t\t*\t*\n*\t*\t*\t*\t*\n*\t*\t\t*\t*\n*\t\t\t\t*");
